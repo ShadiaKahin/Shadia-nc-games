580b1982af1b165ece910599de717eaa17896f33
Shadia Kahin <kahinshadia@gmail.com>
2022-09-05 15:12:30 +0100

    undo changes on main

--- db/data/test-data/index.js
+++ db/data/test-data/index.js
@@ -1,11 +1,4 @@
-const categoryData = require('./categories.js');
-const commentData = require('./comments.js');
-const reviewData = require('./reviews.js');
-const userData = require('./users.js');
-
-module.exports = {
-    categoryData,
-    commentData,
-    reviewData,
-    userData
-}
+exports.categoryData = require('./categories.js');
+exports.commentData = require('./comments.js');
+exports.reviewData = require('./reviews.js');
+exports.userData = require('./users.js');
