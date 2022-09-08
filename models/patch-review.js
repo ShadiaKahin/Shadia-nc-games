266e39b0c028554b4f25002357f18a9a0dfa168a
Shadia Kahin <kahinshadia@gmail.com>
2022-09-08 17:41:40 +0100

    updated the query

--- models/patch-review.js
+++ models/patch-review.js
@@ -3,7 +3,7 @@ const db = require('../db/connection')
 exports.updatedReview = (reviewId, newVote) => {
     return db.query(`UPDATE reviews SET votes = votes + ${newVote}
 WHERE review_id = ${reviewId}
-RETURNING review_id, votes, title, category, designer, owner, review_body, review_img_url, created_at;`
+RETURNING *`
         )
         .then((data) => {
             return data.rows[0]
