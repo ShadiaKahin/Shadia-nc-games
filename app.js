ef566d79d16ab32635a7108461b67a4f20d45e40
Shadia Kahin <kahinshadia@gmail.com>
2022-09-09 16:39:51 +0100

    updated the changed

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ app.get('/api/users', getUsers);
 app.patch('/api/reviews/:review_id', patchReview)
 
 app.use((err, req, res, next) => {
-    if (err.status) {
+    console.log(err)
+    if (err.code) {
+        res.status(400).send({ message: "bad request" })
+    }
+    else if (err.status) {
+        console.log(err.message)
       res.status(err.status).send({ message: err.message })
     }
     else next(err);
@@ -26,7 +31,8 @@ app.use((err, req, res, next) => {
 
 
 app.use((err, req, res, next) => {
-    console.log(err);
+    console.log(err.message)
+
     res.status(500).send({ message: "server error" });
   });
 
--- models/patch-review.js
+++ models/patch-review.js
@@ -1,12 +1,14 @@
 const db = require('../db/connection')
 
 exports.updatedReview = (reviewId, newVote) => {
-    return db.query(`UPDATE reviews SET votes = votes + ${newVote}
-WHERE review_id = ${reviewId}
-RETURNING *`
+    return db.query(`UPDATE reviews SET votes = votes + $1
+WHERE review_id = $2 RETURNING *;`, [newVote, reviewId] 
         )
         .then((data) => {
-            return data.rows[0]
+            console.log(data.rows)
+            if (data.rows.length === 0) {
+              return Promise.reject({ status: 404, message: 'not found'})
+              }
+            return data.rows[0];
         })
- 
 }
